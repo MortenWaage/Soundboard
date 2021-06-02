06671ca73ac6bb3c64b31df2ec8cdbfceaf5a2a6
MortenWaage <84008835+MortenWaage@users.noreply.github.com>
2021-06-02 11:25:30 +0200

    nesten ferdig

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var audio = document.getElementById('myAudio');
+var audio;
+var dyr = [ "audio1", "audio2", "audio3",
+            "audio4", "audio5", "audio6",
+            "audio7", "audio8", "audio9",]
 
-function playSound()
+
+function playSound(_id)
 {
+    if (audio != null)
+        audio.pause();
+
+    audio = document.getElementById(dyr[parseInt(_id)-1]);
     audio.play();
 }
